6b2b24658954af4f530d503d9d8e1340ddc726fa
Hafiz Hamza <hafizhamza4013@gmail.com>
2022-04-22 11:25:01 +0500

    Refactored estore app.

--- estore/views.py
+++ estore/views.py
@@ -1,127 +1,19 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from estore.models import Category, Product, Cart
+from datetime import datetime
+import requests
 import decimal
 import base64
-import requests
-from requests.structures import CaseInsensitiveDict
 import json
-from datetime import datetime
 
-headers = CaseInsensitiveDict()
-headers['accept'] = 'application/json'
-headers['Content-Type'] = 'application/json'
+
 tokens = 'e5f1fdd8-de74-4d38-a66b-e094626d9014:bdcfe703-b344-4f23-87c0-cc34284b9330'
 encoded = base64.b64encode(tokens.encode('ascii'))
-headers['Authorization'] = 'Basic ' + encoded.decode('utf-8')
+headers = {
+     'Content-Type': 'application/json',
+     'Authorization': 'Basic ' + encoded.decode('utf-8')
+}
 
-def verify(card_token, name, lastFour, expiration):
-    #card_token = '9122c6a1-ea32-4238-8d7f-13bb44565ae8'
-
-    url = "https://sandbox-api.marqeta.com/v3/cards/" + card_token
-
-    response = requests.get(url, headers=headers)
-
-    res = json.loads(response.text)
-    if lastFour == res['last_four'] and expiration == res['expiration'] and name == res['fulfillment']['card_personalization']['text']['name_line_1']['value']:
-        return True
-    else:
-        return False
-
-
-def getCardToken(pan, name, lastFour, expiration):
-
-    url = "https://sandbox-api.marqeta.com/v3/cards/getbypan"
-
-    data = """
-    {{
-        "pan": "{0}"
-    }}
-    """
-
-    temp = data.format(pan)
-
-    resp = requests.post(url, headers=headers, data=temp)
-
-    if resp.status_code == 200:
-        res = json.loads(resp.text)
-        if verify(res['card_token'], name, lastFour, expiration):
-            return True, res['card_token']
-    
-    return False, ""
-
-def transaction(pan, name, lastFour, expiration, amount):
-    url = "https://sandbox-api.marqeta.com/v3/simulate/authorization"
-
-    headers = CaseInsensitiveDict()
-    headers['accept'] = 'application/json'
-    headers['Content-Type'] = 'application/json'
-    encoded = base64.b64encode('e5f1fdd8-de74-4d38-a66b-e094626d9014:bdcfe703-b344-4f23-87c0-cc34284b9330'.encode('ascii'))
-    headers['Authorization'] = 'Basic ' + encoded.decode('utf-8')
-
-    mid = '123456890'
-    #card_token = '9122c6a1-ea32-4238-8d7f-13bb44565ae8'
-    status, card_token = getCardToken(pan, name, lastFour, expiration)
-    if status:
-        data = """
-        {{
-            "amount": "{0}", 
-            "mid": "{1}",
-            "card_token": "{2}",
-            "webhook": {{
-                "endpoint": "**URL FOR RECEIVING NOTIFICATIONS**",
-                "username": "**MY USER NAME**",
-                "password": "**MY PASSWORD**"
-            }}
-        }}
-        """
-
-        temp = data.format(amount, mid, card_token)
-
-        resp = requests.post(url, headers=headers, data=temp)
-
-        if resp.status_code == 202:
-            return True, resp.status_code
-        else:
-            return False, resp.status_code
-    else:
-        return False, ""
-
-def checkout(request):
-    if request.method == 'POST':
-        pan = request.POST.get("pan")
-        name = request.POST.get("name")
-        lastFour = request.POST.get("lastFour")
-        expiration =  request.POST.get("expiration")
-        amount = request.POST.get("amount")
-
-        isSuccess, statusCode = transaction(pan, name, lastFour, expiration, amount)
-
-        context = {
-            'isSuccess': isSuccess,
-            'status_code': statusCode,
-            'date': datetime.now().strftime("%A, %B %d, %Y"),
-            'name': name,
-            'pan': pan,
-            'cart_total': amount
-        }
-
-        if isSuccess:
-            context['message'] = 'Your transaction was successfully completed!'
-            return render(request, 'estore/invoice.html', context)
-        else:
-            context['message'] = 'Your transaction could not be completed!'
-            context['lastFour'] = lastFour
-            context['expiration'] = expiration
-            return render(request, 'estore/checkout.html', context)
-
-        # pan = 1111115384669537
-
-    context = {
-        'cart_total': cart_total(),
-        'isSuccess': True
-    }
-
-    return render(request, 'estore/checkout.html', context)
 
 def home(request):
     categories = Category.objects.filter()[:3]
@@ -210,3 +102,84 @@ def decrement_cart(request, cart_id):
             cart_item.quantity -= 1
             cart_item.save()
     return redirect('estore:cart')
+
+def verify(card_token, name, lastFour, expiration):
+    url = "https://sandbox-api.marqeta.com/v3/cards/" + card_token
+
+    response = requests.get(url, headers=headers)
+
+    res = json.loads(response.text)
+    if lastFour == res['last_four'] and expiration == res['expiration'] and name == res['fulfillment']['card_personalization']['text']['name_line_1']['value']:
+        return True
+    return False
+
+def getCardToken(pan, name, lastFour, expiration):
+    url = "https://sandbox-api.marqeta.com/v3/cards/getbypan"
+
+    data = json.dumps({
+        "pan": pan
+    })
+
+    resp = requests.post(url, headers=headers, data=data)
+
+    if resp.status_code == 200:
+        res = json.loads(resp.text)
+        if verify(res['card_token'], name, lastFour, expiration):
+            return True, res['card_token']
+    
+    return False, ""
+
+def transaction(pan, name, lastFour, expiration, amount):
+    url = "https://sandbox-api.marqeta.com/v3/simulate/authorization"
+
+    status, card_token = getCardToken(pan, name, lastFour, expiration)
+
+    if status:
+        data = json.dumps({
+            "amount": amount,
+            "mid": '123456890',
+            "card_token": card_token,
+        })
+
+        resp = requests.post(url, headers=headers, data=data)
+
+        if resp.status_code == 202:
+            return True, resp.status_code
+        return False, resp.status_code
+    
+    return False, ""
+
+def checkout(request):
+    if request.method == 'POST':
+        pan = request.POST.get("pan")
+        name = request.POST.get("name")
+        lastFour = request.POST.get("lastFour")
+        expiration =  request.POST.get("expiration")
+        amount = request.POST.get("amount")
+
+        isSuccess, statusCode = transaction(pan, name, lastFour, expiration, amount)
+
+        context = {
+            'isSuccess': isSuccess,
+            'status_code': statusCode,
+            'date': datetime.now().strftime("%A, %B %d, %Y"),
+            'name': name,
+            'pan': pan,
+            'cart_total': amount
+        }
+
+        if isSuccess:
+            Cart.objects.all().delete()
+            context['message'] = 'Your transaction was successfully completed!'
+            return render(request, 'estore/invoice.html', context)
+        else:
+            context['message'] = 'Your transaction could not be completed!'
+            context['lastFour'] = lastFour
+            context['expiration'] = expiration
+            return render(request, 'estore/checkout.html', context)
+
+    context = {
+        'cart_total': cart_total(),
+        'isSuccess': True
+    }
+    return render(request, 'estore/checkout.html', context)
